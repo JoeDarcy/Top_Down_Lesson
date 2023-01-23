a06ec8864fbdc977f390b1aa3ff0010eb4f73541
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2023-01-23 18:44:14 +0000

    Loaded on PC.

--- Top_Down_Lesson/Assets/Scripts/Bullet.cs
+++ Top_Down_Lesson/Assets/Scripts/Bullet.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private GameObject hitEffect;
+    [SerializeField] private GameObject hitEffectPrefab;
 
     private GameObject hitEffectInstance;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        hitEffectInstance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        hitEffectInstance = Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
         Destroy(hitEffectInstance, 2.0f);
         Destroy(collision.gameObject);
         Destroy(gameObject);
