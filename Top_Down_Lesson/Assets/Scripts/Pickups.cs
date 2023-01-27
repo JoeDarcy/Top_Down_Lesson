c15de491fcf97e286087da0498a086191f1825b0
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2023-01-27 10:02:04 +0000

    Updated muzzle flash code.

--- Top_Down_Lesson/Assets/Scripts/Pickups.cs
+++ Top_Down_Lesson/Assets/Scripts/Pickups.cs
@@ -22,11 +22,12 @@ public class Pickups : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Increase health by 1 on health pickup
         if(collision.gameObject.CompareTag("Health_Pickup"))
         {
             Destroy(collision.gameObject);
 
-            if (PlayerHealth.playerHealth < 10)
+            if (PlayerHealth.playerHealth < PlayerHealth.maxPlayerHealth)
             {
                 PlayerHealth.playerHealth += 1;
             }
